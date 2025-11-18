5ffceb0f33aa1203a58e8b9e3f29e23e378c5aaf
Afsana Mim <afsanamim9639@gmail.com>
2025-11-19 01:51:09 +0600

    Edited server.js file

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
 import statsRoutes from "./routes/statsRoutes.js";
 import challengeRoutes from "./routes/challengeRoutes.js";
 import tipsRoutes from "./routes/tipsRoutes.js";
@@ -10,7 +9,6 @@ import eventRoutes from "./routes/eventRoutes.js";
 import admin from "firebase-admin";
 
 dotenv.config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -35,11 +33,10 @@ export const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer "))
     return res.status(401).json({ message: "No token provided" });
-
   const token = authHeader.split(" ")[1];
   try {
     const decodedToken = await admin.auth().verifyIdToken(token);
-    req.user = decodedToken; // contains uid, email, etc.
+    req.user = decodedToken;
     next();
   } catch (err) {
     console.error("Token verification error:", err);
@@ -53,11 +50,10 @@ app.use("/api/stats", statsRoutes);
 app.use("/api/tips", tipsRoutes);
 app.use("/api/events", eventRoutes);
 
-// Optional: Root route
+// Root route
 app.get("/", (req, res) => {
   res.send("🌿 EcoTrack Backend is running!");
 });
-
 
 // Start server
 const PORT = process.env.PORT || 5000;
